74551506ed5ebae5068d2de9a413d33137a673bd
XurshidJurayev1 <xjurayev99@gmail.com>
2022-11-25 18:29:53 +0500

    change format sum datatable

--- src/helpers/formatSum2.js
+++ src/helpers/formatSum2.js
@@ -17,8 +17,29 @@ export const formatSum = (value) => {
   if (phoneNumberLength === 7) {
     return `${phoneNumber.slice(0, 1)} ${phoneNumber.slice(1, 4)} ${phoneNumber.slice(4, 7)}`;
   }
-  if (phoneNumberLength < 9) {
+  if (phoneNumberLength === 8) {
     return `${phoneNumber.slice(0, 2)} ${phoneNumber.slice(2, 5)} ${phoneNumber.slice(5)} `;
   }
-  return `${phoneNumber.slice(0, 3)} ${phoneNumber.slice(3, 6)} ${phoneNumber.slice(6, 9)}  `;
+  if (phoneNumberLength === 9) {
+    return `${phoneNumber.slice(0, 3)} ${phoneNumber.slice(3, 6)} ${phoneNumber.slice(6)} `;
+  }
+  if (phoneNumberLength === 10) {
+    return `${phoneNumber.slice(0, 1)} ${phoneNumber.slice(1, 4)} ${phoneNumber.slice(4, 7)} ${phoneNumber.slice(7)} `;
+  }
+  if (phoneNumberLength === 11) {
+    return `${phoneNumber.slice(0, 2)} ${phoneNumber.slice(2, 5)} ${phoneNumber.slice(5, 8)} ${phoneNumber.slice(8)} `;
+  }
+  if (phoneNumberLength === 12) {
+    return `${phoneNumber.slice(0, 3)} ${phoneNumber.slice(3, 6)} ${phoneNumber.slice(6, 9)} ${phoneNumber.slice(9)} `;
+  }
+  if (phoneNumberLength === 13) {
+    return `${phoneNumber.slice(0, 1)} ${phoneNumber.slice(1, 4)} ${phoneNumber.slice(4, 7)} ${phoneNumber.slice(7, 10)} ${phoneNumber.slice(10)} `;
+  }
+  if (phoneNumberLength === 14) {
+    return `${phoneNumber.slice(0, 2)} ${phoneNumber.slice(2, 5)} ${phoneNumber.slice(5, 8)} ${phoneNumber.slice(8, 11)} ${phoneNumber.slice(11)} `;
+  }
+  if (phoneNumberLength === 15) {
+    return `${phoneNumber.slice(0, 3)} ${phoneNumber.slice(3, 6)} ${phoneNumber.slice(6, 9)} ${phoneNumber.slice(9, 12)} ${phoneNumber.slice(12)}`;
+  }
+  return `${phoneNumber.slice(0, 3)} ${phoneNumber.slice(3, 6)} ${phoneNumber.slice(6, 9)} ${phoneNumber.slice(9, 12)} ${phoneNumber.slice(12)}`;
 };
